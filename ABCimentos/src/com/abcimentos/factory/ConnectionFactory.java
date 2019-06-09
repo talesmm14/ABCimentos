c52fddac5377a34506deab01c449bdecab9cdb70
talesmm14 <talesmelquiades@hotmail.com>
2019-06-09 01:11:43 -0300

    Fixing a connection error with the database.

--- ABCimentos/src/com/abcimentos/factory/ConnectionFactory.java
+++ ABCimentos/src/com/abcimentos/factory/ConnectionFactory.java
@@ -18,7 +18,7 @@ public class ConnectionFactory {
 			// registrando o driver do postgres
 			Class.forName("org.postgresql.Driver");
 			// estabelecendo um conexao com o banco
-			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5433/abcimentos",
+			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/abcimentos",
 															"topicos1", "123456");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Falha ao registrar o Driver de banco");
