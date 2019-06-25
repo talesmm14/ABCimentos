9da4856f5b6fdb4b5a3a179d1a237c7a32871366
Mateus Asevedo <mateusasevedo@gmail.com>
2019-06-25 17:40:02 -0300

    mudança nas telas

--- ABCimentos/src/com/abcimentos/controller/ConsultaUsuarioController.java
+++ ABCimentos/src/com/abcimentos/controller/ConsultaUsuarioController.java
@@ -44,7 +44,7 @@ public class ConsultaUsuarioController  implements Serializable {
 		Usuario usuario = dao.findById(id);
 		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
 		flash.put("usuarioFlash", usuario);
-		Util.redirect("cadastroUsuario.xhtml");
+		Util.redirect("cadastroUsuarios.xhtml");
 	}
 
 	public String getNome() {
